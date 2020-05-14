d326c821d4dd1cc75f7a2ee387b51b3726bb2fba
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2020-05-14 14:43:11 +0200

    Ensure that both expression targets and operands are traversed

--- util/FormulaTraversal.cs
+++ util/FormulaTraversal.cs
@@ -11,19 +11,15 @@ namespace smtsharp.Util
         public Formula Formula { get; }
 
 
-        public FormulaTraversal(Formula formula)
-        {
+        public FormulaTraversal(Formula formula) =>
             Formula = formula;
-        }
 
         public void Apply(Action action)
         {
             ISet<IExpression<Type>> visited = new HashSet<IExpression<Type>>();
 
-            foreach (var expr in Formula.Declarations)
-            {
+            foreach (var expr in Formula.Declarations) 
                 Apply(action, expr, visited);
-            }
         }
 
         private static void Apply(Action action, IExpression<Type> expression, ISet<IExpression<Type>> visited)
@@ -33,9 +29,9 @@ namespace smtsharp.Util
                 action(expression);
                 visited.Add(expression);
                 foreach (var target in expression.Targets())
-                {
                     Apply(action, target, visited);
-                }
+                foreach (var operand in expression.Operands())
+                    Apply(action, operand, visited);
             }
         }
     }
